0bec377484e8d73e9a29b0b1d3d32204ea138f68
Konrad Zadroga <konradzadroga@macbook-pro-konrad.home>
2021-02-20 00:34:58 +0100

    First version of script

---
+++ imgur_uploader.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+file=$1
+client_id=$2
+
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+file_name=$(get_abs_filename $file)
+
+if [ $# -lt 2 ]; then
+	echo "You have to pass argument"
+else
+	if [[ $file == *.jpg || $file == *.png || $file == *.tiff ]]; then
+		#echo $file_name	
+		link=$(curl --request POST --url https://api.imgur.com/3/image \
+		--header 'authorization: Client-ID '$client_id'' \
+		--header 'content-type: multipart/form-data;' \
+		 -F "image=@$file_name" --silent | jq -r '.data.link')
+		echo $link
+	else
+		echo "File must be an image!"
+	fi
+fi
